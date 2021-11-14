2d1f7481f5b5794cdf2fc811fc896b7517557424
timor23 <timors23@protonmail.com>
2021-11-14 08:46:55 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -75,7 +75,30 @@ app.put('/:action/:accountNum', (req, res) => {
             break;
     }
     fs.writeFileSync(clientsFile, JSON.stringify(buffer));
-    return res.status(200).json({success: 'Item deleted successfully'})
+    return res.status(200).json({success: 'OK'});
+})
+
+app.put('./transfer/:fromAccount/:toAccount', (rew,res)=>{
+    const {fromAccount, toAccount} = req.params;
+    let buffer = JSON.parse(fs.readFileSync(clientsFile).toString());
+    const account1 = buffer.find((client) => {
+        return fromAccount == client.account
+    });
+    const account2 = buffer.find((client) => {
+        return toAccount == client.account
+    });
+
+    if (!account1.isActive || !account2.isActive) {
+        return res.status(404).send('Account is not active');
+    }
+    if ((account2.balance + account2.credit) < req.body.amount) {
+        return res.status(404).send('Not enough money');
+    }
+    account1.balance -= req.body.amount;
+    account2.balance += req.body.amount;
+
+    fs.writeFileSync(clientsFile, JSON.stringify(buffer));
+    return res.status(200).json({success: 'OK'});
 })
 
 app.listen(5001, () => {
